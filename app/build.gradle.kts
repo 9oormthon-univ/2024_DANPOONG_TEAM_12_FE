7c9d758d962cfc3f7101d7b9c2deaf3cdee41ba7
Geonheu <geonhui0987@gmail.com>
2024-11-18 11:10:18 +0900

    [Feat] 카카오 로그인 기능 구현

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+import java.util.Properties
+
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+    //	Proerties 표준함수명이 Java > kotlin 으로 넘어오면서 변함.
+    //.inputStream() 부분도 표준 함수명이 변해 kotlin 환경에서 사용하시는분들은 이렇게 참고해서 사용하시면 됩니다.
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
+
+android {
+    namespace = "com.example.cospick"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.cospick"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "NATIVE_KEY",
+            properties.getProperty("native_key")
+        )
+        manifestPlaceholders["NATIVE_KEY"] = properties.getProperty("manifest_native_key")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    // retrofit
+
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Kakao SDK 추가
+    implementation("com.kakao.sdk:v2-all:2.20.1")
+    implementation("com.kakao.sdk:v2-user:2.20.1")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/cospick/onBoarding/LoginActivity.kt
@@ -0,0 +1,90 @@
+package com.example.cospick.onBoarding
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cospick.R
+import com.example.cospick.databinding.ActivityLoginBinding
+import com.example.cospick.utils.Constants.TAG
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    companion object {
+        fun newInstance() = LoginActivity()
+    }
+    // 카카오 로그인
+    // 카카오 계정으로 로그인 공통 callback 구성
+    // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경 사용됨
+    val callback: (OAuthToken?, Throwable?) -> Unit = {token, error ->
+        if (error != null){
+            Log.d(TAG, "로그인 실패 ${error.localizedMessage}")
+        }
+        else if (token != null){
+            Log.d(TAG, "로그인 성공 ! ${token.accessToken} ")
+            GoMain()
+        }
+    }
+
+    private fun initView() {
+        val context: Context = this
+        //  버튼 클릭했을 때 로그인
+        with(binding){
+            kakaoLoginBtn.setOnClickListener {
+                Log.d(TAG, "버튼 클릭 되긴 함 ㅋㅋ")
+
+                // 카카오톡이 설치되어 있으면 카카오톡 로그인, 아니면 카카오계정 로그인
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                    UserApiClient.instance.loginWithKakaoTalk(context){ token, error ->
+                        if (error != null) {
+                            Log.d(TAG, "initView: 로그인 실패! ${error.localizedMessage}")
+
+                            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                            // 의도적인 로그인 취소로 보 카카오계정으로 로그인 시도 없이 로그인 취소 처리 (예 : 뒤로가기)
+                            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                                return@loginWithKakaoTalk
+                            }
+                        }
+                        else if (token != null) {
+                            Log.d(TAG, "initView: 로그인 성공 ${token.accessToken}")
+                            GoMain()
+                        }
+                    }
+                }
+                else {
+                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+
+        Log.d("KeyHash", "${Utility.getKeyHash(this)}")
+    }
+
+    private fun GoMain(){
+        // 로그인 -> 온보딩 액티비티
+        // OnBoardingActivity로 이동
+        val intent = Intent(this, OnBoardingActivity::class.java)
+        startActivity(intent)
+        finish() // 현재 액티비티 종료
+    }
+}
