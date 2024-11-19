396143b74079c725379ef3f3c2f542607d905460
naals <nmh8888@naver.com>
2024-11-20 02:38:31 +0900

    [Feat] home_xml

--- app/src/main/java/com/example/waytogo/HomeActivity.kt
+++ app/src/main/java/com/example/waytogo/HomeActivity.kt
@@ -1,20 +1,35 @@
 package com.example.waytogo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // 상태바와 콘텐츠 충돌 방지
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+        val topBar = findViewById<View>(R.id.homeTopBar_fl)
+
+        // WindowInsets를 사용해 상태바 높이를 강제로 적용
+        ViewCompat.setOnApplyWindowInsetsListener(topBar) { view, insets ->
+            val statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top
+            Log.d("Insets", "StatusBar height: $statusBarHeight")
+
+            // FrameLayout에 패딩 적용
+            view.setPadding(0, statusBarHeight, 0, 0)
             insets
         }
+
+        // 상태바 색상 설정
+        window.statusBarColor = resources.getColor(R.color.blue, theme)
     }
 }
